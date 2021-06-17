941dda7511649ef1ad3dcb3c0315d61b68e57f09
Administrator <Administrator@SELDA-LAPTOP>
2021-06-17 23:03:51 +0300

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   
+    cout << "Hello World!\n";
+    int a;
+    a = 5;
+    int x;
+    if (true) cout << x;
+    //deneme
+    return 0;
+}
